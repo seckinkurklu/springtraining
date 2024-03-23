e0e0145eebd3f71c0fcc3d214b3243097574b3fb
seckinkurklu <kurkluseckin@gmail.com>
2024-03-23 14:52:30 +0300

    spring-00-Core-IoC-Bean-Annotation

--- spring-00-Coupling/src/main/java/com/cydeo/loosely_coupled/Main.java
+++ spring-00-Coupling/src/main/java/com/cydeo/loosely_coupled/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Monitor theMonitor = new AcerMonitor("27inch Beast", "Acer", 27);
+        Monitor theMonitor = new AcerMonitor("27 inch Beast", "Acer", 27);
         Case theCase = new DellCase("220B", "Dell", "240");
         Motherboard theMotherboard = new AsusMotherboard("BJ-200", "Asus", 4, 6, "v2.44");
 
