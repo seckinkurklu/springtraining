2a86eceb89f3327529589460f21287c220c7b17e
seckinkurklu <kurkluseckin@gmail.com>
2024-05-03 09:48:39 +0300

    Spring Data

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,17 +1,32 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "students")
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "studentFirstName")
     private String firstName;
+    @Column(name = "studentLastName")
     private String lastName;
+    @Column(name = "studentEmail")
     private String email;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthDate;
+    @Column(columnDefinition = "TIME")
+
+    private LocalTime birthTime;
+    @Column(columnDefinition = "TIMESTAMP")
+
+    private LocalDateTime birthDateTime;
+
 
 }
