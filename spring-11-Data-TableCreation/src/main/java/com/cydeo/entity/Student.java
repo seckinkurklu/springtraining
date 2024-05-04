ccf662af8198206ddff5376e1867ecd93293dccc
seckinkurklu <kurkluseckin@gmail.com>
2024-05-04 15:58:38 +0300

    Spring Data

--- spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,5 +1,7 @@
 package com.cydeo.entity;
 
+import com.cydeo.enums.Gender;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -27,6 +29,12 @@ public class Student {
     @Column(columnDefinition = "TIMESTAMP")
 
     private LocalDateTime birthDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Transient
+    private String city;
 
 
 }
